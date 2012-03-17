6fd7a6d595d582468fa751d6daf08bb68f37b70b
Vincent Paca <vincent.paca@gmail.com>
2012-03-17 23:22:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopology::Application.config.secret_token = '3c0f09730947f2ee056ccba1afe6047a4e4e6404b8b2d8d49230e3d9be43293dd60fbce64e4a94ae21cdcb613a32d499ad1f42d6d40f09aff4112117fd471b95'
